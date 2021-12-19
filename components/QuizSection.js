9ecdfa99576800e73a1bc6daf6eba3f2ea346924
Azad Ansari <azadansari@Azads-MacBook-Air.local>
2021-12-19 20:19:44 +0530

    initial commit

---
+++ components/QuizSection.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import styles from "@/styles/QuizSection.module.css";
+
+export default function QuizSection(props) {
+  const { question, options } = props;
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (e) => {
+    const selected = e.target.getAttribute("data-option");
+    if (selected) {
+      setSelectedOption(selected);
+    }
+  };
+
+  return (
+    <Box>
+      <div className={styles.question}>
+        We can update the state in React.js by calling to setState() method.
+        These calls are:
+      </div>
+
+      <Grid
+        container
+        spacing={2}
+        sx={{ display: "flex", justifyContent: "center" }}
+        onClick={handleSelect}
+      >
+        <Grid item md={6}>
+          <div
+            className={
+              selectedOption === "a"
+                ? `${styles.option} ${styles.selected}`
+                : styles.option
+            }
+            data-option="a"
+          >
+            A. Synchronous in nature.
+          </div>
+        </Grid>
+
+        <Grid item md={6}>
+          <div
+            className={
+              selectedOption === "b"
+                ? `${styles.option} ${styles.selected}`
+                : styles.option
+            }
+            data-option="b"
+          >
+            B. Asynchronous in nature.
+          </div>
+        </Grid>
+
+        <Grid item md={6}>
+          <div
+            className={
+              selectedOption === "c"
+                ? `${styles.option} ${styles.selected}`
+                : styles.option
+            }
+            data-option="c"
+          >
+            C. Are asynchronous but can be made synchronous when required.
+          </div>
+        </Grid>
+
+        <Grid item md={6}>
+          <div
+            className={
+              selectedOption === "d"
+                ? `${styles.option} ${styles.selected}`
+                : styles.option
+            }
+            data-option="d"
+          >
+            D. None of the above.
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ theme/index.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#585ff2",
+      light: "#acb1f9",
+      dark: "#393fc2",
+    },
+  },
+});
